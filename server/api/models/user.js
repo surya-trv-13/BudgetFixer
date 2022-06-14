4381cd26cff50029e11f6dc89b00edfa7fd4b375
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2022-06-14 23:18:32 +0530

    server | querying the transaction

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,34 +1,35 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true.value,
-        trim: true,
-        validate: {
-            validator: validator.isEmail,
-            message: "Email is Invalid",
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: Number,
-        required: false,
-        validate: {
-            validator: validator.isPhone,
-            message: "Mobile Number is invalid",
-        },
-    },
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true.value,
+		trim: true,
+		validate: {
+			validator: validator.isEmail,
+			message: "Email is Invalid",
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: Number,
+		required: false,
+		validate: {
+			validator: validator.isPhone,
+			message: "Mobile Number is invalid",
+		},
+	},
 });
 
 const User = mongoose.model("User", userSchema);
--- server/api/server.js
+++ server/api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./database/mongoose");
 
 const { Transaction } = require("./models/transactions");
+// const { User } = require("./models/user");
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +11,30 @@ app.use(express.json());
 
 app.get("/health", (req, res) => {
 	res.send("Connection Working : " + req.body.user);
+});
+
+app.get("/transaction", async (req, res) => {
+	Transaction.find({
+		$and: [
+			{
+				$expr: {
+					$eq: [{ $month: "$transactionDate" }, 6],
+				},
+			},
+			{
+				$expr: {
+					$eq: [{ $year: "$transactionDate" }, 2022],
+				},
+			},
+		],
+	})
+		.then((transaction) => {
+			console.log("Result is", transaction);
+			res.send(transaction);
+		})
+		.catch((e) => {
+			res.status(400).send(e);
+		});
 });
 
 app.post("/transaction", async (req, res) => {
